009de8a9b8d2a07b1a8fe7fa74bebd663c670e30
Will <you@example.com>
2020-07-25 11:37:59 +0100

    added viewports

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
 // .storybook/main.js
 module.exports = {
   stories: ['../src/components/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', 
-           '@storybook/addon-links', 
-           '@storybook/addon-knobs/register']
+  addons: [
+    '@storybook/addon-actions', 
+    '@storybook/addon-links', 
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
+  ]
 };
