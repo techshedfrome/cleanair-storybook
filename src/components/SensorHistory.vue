0600e9105f31e91c58af31fc2f90739c7a070e30
Will <you@example.com>
2020-08-15 12:45:08 +0100

    updated notes

--- src/components/SensorHistory.vue
+++ src/components/SensorHistory.vue
@@ -10,6 +10,20 @@
 <script>
 /*
 
+potential alternative visualisations:
+  number of minutes in each band
+  max of contiguous time in each band
+  over period, stacked percentage chart of each band
+
+  exploration:
+    correlation - pick specific time windows and filter visualisations to that
+      enables estimation of personal exposure based on time spent in area
+
+    comparison between sensors in town
+      add as multiple series to chart
+
+
+
 TODO: Graphing view - likely powered by D3.js
 
 initial example from: https://alligator.io/vuejs/visualization-vue-d3/
@@ -44,6 +58,7 @@ https://vuejsexamples.com/a-component-of-chartist-implement-by-vuejs-2-0/
 https://vuejsexamples.com/simple-and-elegant-spark-bars-for-vue-js/
 
 https://vuejsexamples.com/echarts-with-vue2-0/
+  https://translate.google.co.uk/translate?um=1&ie=UTF-8&hl=en&client=tw-ob&sl=zh-CN&tl=en&u=https%3A%2F%2Fgithub.com%2FSimonZhangITer%2FDataVisualization
 
 */
 import * as d3 from "d3";
